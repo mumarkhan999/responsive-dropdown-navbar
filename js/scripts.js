49a08164931c206e18beba6a3f2ca7f39f8a005e
M Umar Khan <umar.khan@arbisoft.com>
2021-07-07 21:38:57 +0500

    Add sliding dropdown on all main menu items

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
 const solutionsButton = document.getElementById('solutions')
 const solutionsSection = document.getElementsByClassName('solutions')[0]
 
-solutionsButton.addEventListener('click', () => {
+const resourcesButton = document.getElementById('resources')
+const resourcesSection = document.getElementsByClassName('resources')[0]
+
+const whyEdlyButton = document.getElementById('why-edly')
+const whyEdlySection = document.getElementsByClassName('why-edly')[0]
+
+
+
+solutionsButton.addEventListener('mouseover', () => {
     solutionsSection.classList.toggle('header-swipe')
-    console.log('hahaha');
+})
+
+solutionsButton.addEventListener('mouseout', () => {
+    solutionsSection.classList.toggle('header-swipe')
+})
+
+
+resourcesButton.addEventListener('mouseover', () => {
+    resourcesSection.classList.toggle('header-swipe')
+})
+
+resourcesButton.addEventListener('mouseout', () => {
+    resourcesSection.classList.toggle('header-swipe')
+})
+
+
+whyEdlyButton.addEventListener('mouseover', () => {
+    whyEdlySection.classList.toggle('header-swipe')
+})
+
+whyEdlyButton.addEventListener('mouseout', () => {
+    whyEdlySection.classList.toggle('header-swipe')
 })
