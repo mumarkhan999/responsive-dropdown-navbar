31bd2969dbcc9b645088afe92b3c18e664215a25
M Umar Khan <umar.khan@arbisoft.com>
2021-07-08 00:35:02 +0500

    Add hamburger

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,42 @@
-const solutionsButton = document.getElementById('solutions')
-const solutionsSection = document.getElementsByClassName('solutions')[0]
+const solutionsButton = document.getElementById('solutions');
+const solutionsSection = document.getElementsByClassName('solutions')[0];
 
-const resourcesButton = document.getElementById('resources')
-const resourcesSection = document.getElementsByClassName('resources')[0]
+const resourcesButton = document.getElementById('resources');
+const resourcesSection = document.getElementsByClassName('resources')[0];
 
-const whyEdlyButton = document.getElementById('why-edly')
-const whyEdlySection = document.getElementsByClassName('why-edly')[0]
+const whyEdlyButton = document.getElementById('why-edly');
+const whyEdlySection = document.getElementsByClassName('why-edly')[0];
 
+
+const hamBurgerButton = document.getElementsByClassName('ham-burger')[0];
 
 
 solutionsButton.addEventListener('mouseover', () => {
-    solutionsSection.classList.toggle('header-swipe')
-})
+    solutionsSection.classList.toggle('header-swipe');
+});
 
 solutionsButton.addEventListener('mouseout', () => {
-    solutionsSection.classList.toggle('header-swipe')
-})
+    solutionsSection.classList.toggle('header-swipe');
+});
 
 
 resourcesButton.addEventListener('mouseover', () => {
-    resourcesSection.classList.toggle('header-swipe')
-})
+    resourcesSection.classList.toggle('header-swipe');
+});
 
 resourcesButton.addEventListener('mouseout', () => {
-    resourcesSection.classList.toggle('header-swipe')
-})
+    resourcesSection.classList.toggle('header-swipe');
+});
 
 
 whyEdlyButton.addEventListener('mouseover', () => {
-    whyEdlySection.classList.toggle('header-swipe')
-})
+    whyEdlySection.classList.toggle('header-swipe');
+});
 
 whyEdlyButton.addEventListener('mouseout', () => {
-    whyEdlySection.classList.toggle('header-swipe')
-})
+    whyEdlySection.classList.toggle('header-swipe');
+});
+
+hamBurgerButton.addEventListener('click', () => {
+    hamBurgerButton.classList.toggle('clicked-ham-burger');
+});
