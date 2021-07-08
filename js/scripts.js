4652e581e03e94d28a73a84f82ca61a7983b0d6e
M Umar Khan <umar.khan@arbisoft.com>
2021-07-08 06:00:19 +0500

    Add jquery fix for responsive screen

--- js/scripts.js
+++ js/scripts.js
@@ -13,47 +13,108 @@ const mainHeadings = document.getElementsByClassName('main-headings')[0];
 
 
 
-$( "#solutions" )
-  .mouseover(function() {
-    solutionsSection.classList.add('header-swipe');
-    resourcesSection.classList.remove('header-swipe');
-    whyEdlySection.classList.remove('header-swipe');
-});
+window.onresize = reportWindowSize;
 
-$( ".solutions" )
-  .mouseleave(function() {
-    solutionsSection.classList.remove('header-swipe');
-});
+reportWindowSize();
 
-
-$( "#resources" )
-  .mouseover(function() {
-    solutionsSection.classList.remove('header-swipe');
-    resourcesSection.classList.add('header-swipe');
-    whyEdlySection.classList.remove('header-swipe');
-});
-
-$( ".resources" )
-  .mouseleave(function() {
-    resourcesSection.classList.remove('header-swipe');
-});
+function reportWindowSize() {
+    if (window.innerWidth <= 1275) {
+        addEventListenersForMobile();
+    } else {
+        addEventListenersForDesktop();
+    }
+}
 
 
+function addEventListenersForMobile () {
+    $( "#solutions" ).off();
+    $( "#solutions" )
+    .click(function() {
+        this.children[0].classList.toggle('mobile-drop-btn-inverted');
+        resourcesSection.classList.remove('header-swipe');
+        whyEdlySection.classList.remove('header-swipe');
+        solutionsSection.classList.toggle('header-swipe');
+    });
 
-$( "#why-edly" )
-  .mouseover(function() {
-    solutionsSection.classList.remove('header-swipe');
-    resourcesSection.classList.remove('header-swipe');
-    whyEdlySection.classList.add('header-swipe');
-});
+    $( ".solutions" ).off();
 
-$( ".why-edly" )
-  .mouseleave(function() {
-    whyEdlySection.classList.remove('header-swipe');
-});
+
+    $( "#resources" ).off();
+    $( "#resources" )
+    .click(function() {
+        this.children[0].classList.toggle('mobile-drop-btn-inverted');
+        solutionsSection.classList.remove('header-swipe');
+        whyEdlySection.classList.remove('header-swipe');
+        resourcesSection.classList.toggle('header-swipe');
+    });
+
+    $( ".resources" ).off();
+
+
+    $( "#why-edly" ).off();
+    $( "#why-edly" )
+    .click(function() {
+        this.children[0].classList.toggle('mobile-drop-btn-inverted');
+        solutionsSection.classList.remove('header-swipe');
+        resourcesSection.classList.remove('header-swipe');
+        whyEdlySection.classList.toggle('header-swipe');
+    });
+
+    $( ".why-edly" ).off();
+
+}
+
+
+function addEventListenersForDesktop () {
+    $( "#solutions" ).off();
+    $( "#solutions" )
+    .mouseover(function() {
+        solutionsSection.classList.add('header-swipe');
+        resourcesSection.classList.remove('header-swipe');
+        whyEdlySection.classList.remove('header-swipe');
+    });
+
+    $( "#solutions" )
+    .mouseleave(function() {
+        solutionsSection.classList.remove('header-swipe');
+    });
+
+
+    $( "#resources" ).off();
+    $( "#resources" )
+    .mouseover(function() {
+        solutionsSection.classList.remove('header-swipe');
+        resourcesSection.classList.add('header-swipe');
+        whyEdlySection.classList.remove('header-swipe');
+    });
+
+    $( "#resources" )
+    .mouseleave(function() {
+        resourcesSection.classList.remove('header-swipe');
+    });
+
+
+    $( "#why-edly" ).off();
+    $( "#why-edly" )
+    .mouseover(function() {
+        solutionsSection.classList.remove('header-swipe');
+        resourcesSection.classList.remove('header-swipe');
+        whyEdlySection.classList.add('header-swipe');
+    });
+
+    $( "#why-edly" )
+    .mouseleave(function() {
+        whyEdlySection.classList.remove('header-swipe');
+    });
+
+}
+
+
+
 
 
 hamBurgerButton.addEventListener('click', () => {
     hamBurgerButton.classList.toggle('clicked-ham-burger');
     mainHeadings.classList.toggle('mobile-main-headings');
+    $(".header").toggleClass("header-expanded");
 });
